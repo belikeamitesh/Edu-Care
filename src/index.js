f19c27dff0ca73da196378ba10f0e60f8d306cbf
Amitesh Kumar <56907437+belikeamitesh@users.noreply.github.com>
2022-03-06 17:31:20 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+  <Auth0Provider
+  domain={process.env.REACT_APP_AUTH0_DOMAIN} clientId={process.env.REACT_APP_AUTH0_CLIENT_ID} redirectUri={window.location.origin}
+  >
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
+);
